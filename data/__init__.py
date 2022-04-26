31fc012d8a4e185219d1c3faa3f7878cbae6609c
Maksim Leali <maksimleali@Maksims-Mac-mini.local>
2022-04-26 12:20:05 +0200

    fix: requirments for PyYaml

--- data/__init__.py
+++ data/__init__.py
@@ -3,9 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 import yaml
 
 with open("config.yml", "r") as ymlfile:
-    cfg = yaml.loadÒ(ymlfile)
+    cfg = yaml.load(ymlfile)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{cfg['db']['usr']}:{cfg['db']['password']}@{cfg['db']['host']}:{cfg['db']['port']}/{cfg['db']['table']}"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{cfg['db']['user']}:{cfg['db']['password']}@{cfg['db']['host']}:{cfg['db']['port']}/{cfg['db']['table']}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
