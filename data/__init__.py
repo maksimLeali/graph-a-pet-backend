a9f4c9e0a5fc33866486d1d6d5d8881cfa254392
Maksim Leali <maksimleali@Maksims-Mac-mini.local>
2022-04-26 11:54:40 +0200

    feat: wip config file

--- data/__init__.py
+++ data/__init__.py
@@ -1,8 +1,11 @@
 from api import app
 from flask_sqlalchemy import SQLAlchemy
+import yaml
 
+with open("config.yml", "r") as ymlfile:
+    cfg = yaml.loadÒ(ymlfile)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:147852makso@localhost:5433/pet-finder"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{cfg['db']['usr']}:{cfg['db']['password']}@{cfg['db']['host']}:{cfg['db']['port']}/{cfg['db']['table']}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
