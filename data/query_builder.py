ef46ad65e37166290199d9bc5a626e8cd1da1608
Maksim Leali <maksimleali@Maksims-Mac-mini.local>
2022-05-06 14:57:43 +0200

    feat: wip query builder

---
+++ data/query_builder.py
@@ -0,0 +1,89 @@
+from typing import Dict
+import pydash as py_
+from libs.utils import camel_to_snake
+from itertools import permutations
+
+deafult_search_columns: dict = {
+    "users": ['first_name', 'last_name', 'email', ],
+    "pets": ["name", "race"],
+    "coats": ['pattern'],
+    "ownerships": ['role']
+}
+
+
+def format_range_filters(filters: Dict[str, Dict[str, str ]]) -> str :
+    formatted_filters= ""
+    try :
+        for i, key in enumerate(filters.keys(), start=1):
+            formatted_filters += \
+                f"{camel_to_snake(key)}" \
+                f"{(' > ' + str(filters[key]['min']) ) if  'min' in filters[key] else '' } " \
+                f"{' AND ' + camel_to_snake(key) if len(filters[key]) > 1 else '' }" \
+                f"{(' < ' + str(filters[key]['max']) ) if  'max' in filters[key] else '' } " \
+                f"{' AND ' if i < len(filters.keys()) else '' }"
+    except Exception as e:
+        print(e)            
+    return formatted_filters
+
+def format_fixed_filters(filters: Dict[str,str]) -> str :
+    formatted_filters= ""
+    try :
+        for i, key in enumerate(filters.keys(), start=1):
+            formatted_filters += f"{camel_to_snake(key)} = '{filters[key]}' {'AND' if i < len(filters.keys()) else '' } "
+    except Exception as e:
+        print(e)            
+    return formatted_filters
+
+def format_list_filters(filters: Dict[str, list]) -> str:
+    formatted_filters= ""
+    try :
+        for i, key in enumerate(filters.keys(), start=1):
+            separetor= "', '"
+            formatted_filters += f"{camel_to_snake(key)} in ('{ separetor.join(filters[key])}') {'AND' if i < len(filters.keys()) else '' } "
+    except Exception as e:
+        print(e)            
+    return formatted_filters
+    
+
+def build_where(filters: Dict[str, dict], search: str, search_fields: list) -> str:
+    
+    filters_to_format= []
+    formatted_list_filters= format_list_filters(filters["lists"])
+    formatted_fixed_filters= format_fixed_filters(filters["fixeds"])
+    formatted_range_filters= format_range_filters(filters['ranges'])
+    formatted_search = ""
+    search_list = []
+    
+    print(formatted_fixed_filters)
+    print(formatted_list_filters)
+    print(formatted_range_filters)
+    if len(formatted_fixed_filters) > 0 :
+        filters_to_format.append(formatted_fixed_filters)
+    if len(formatted_list_filters) > 0 :
+        filters_to_format.append(formatted_list_filters)
+    if len(formatted_range_filters) > 0 :
+        filters_to_format.append(formatted_range_filters)
+    formatted_filters = ' AND '.join(filters_to_format)
+    
+    print(formatted_filters)
+    
+    if len(search) > 0:
+        to_permutate= search.split(' ')
+        for i in range(len(to_permutate)) :
+            search_list.extend([" ".join(val) for val in permutations(to_permutate, i+1)])
+    
+    for i, value in enumerate(search_list, start=1):
+        for k, field in enumerate(search_fields, start=1):
+            formatted_search += f"{'(' if i==1 and k==1 else ''} "  \
+                f"LOWER({camel_to_snake(field)}) LIKE LOWER('%{value}%') "  \
+                f"{'OR' if i <  len(search_list) or k < len(search_fields) else ')'} "
+    return f"{'WHERE ' if filters or search else ''}{ formatted_filters}"  \
+            f"{'AND' if len(formatted_filters) > 0 and len(formatted_search) > 0  else ''} {formatted_search}"
+
+
+def build_simple_query(table: str, search, search_fields, pagination: Dict[str, int], ordering: Dict[str, str], filters: dict = {"fixeds" : None, "lists": None, "ranges": None} ):
+    return f"SELECT *" \
+           f"FROM {table}" \
+           f" {build_where( filters,search,  search_fields if len(search_fields)> 0 else deafult_search_columns[table]) }" \
+           f"ORDER BY {ordering['order_by']} {ordering['order_direction'].upper()}, id ASC " \
+           f"LIMIT {pagination['page_size']} OFFSET {pagination['page_size'] * pagination['page']}"
