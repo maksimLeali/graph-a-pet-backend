f2b5187e386caa682a817b8570e64df076a39374
Maksim Leali <maksimleali@Maksims-Mac-mini.local>
2022-06-21 11:56:17 +0200

    feat: database setup

--- data/__init__.py
+++ data/__init__.py
@@ -1,8 +1,9 @@
 from api import app
+from os import getenv
 from flask_sqlalchemy import SQLAlchemy
 from config import cfg 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{cfg['db']['user']}:{cfg['db']['password']}@{cfg['db']['host']}:{cfg['db']['port']}/{cfg['db']['table']}"
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL", "sqlite://")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
