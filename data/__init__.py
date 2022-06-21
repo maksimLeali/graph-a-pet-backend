3e9f40306008a6c5bdb4bb3b93f70f6d6d5ff287
Maksim Leali <maksimleali@Maksims-Mac-mini.local>
2022-06-21 13:52:20 +0200

    fi

--- data/__init__.py
+++ data/__init__.py
@@ -1,7 +1,11 @@
 from api import app
 from flask_sqlalchemy import SQLAlchemy
 from config import cfg 
+import os 
 
+print(os.getenv('DATABASE_URL', "postgresql://"))
+print("################")
+print(f"postgresql://{cfg['db']['user']}:{cfg['db']['password']}@{cfg['db']['host']}:{cfg['db']['port']}/{cfg['db']['table']}")
 app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{cfg['db']['user']}:{cfg['db']['password']}@{cfg['db']['host']}:{cfg['db']['port']}/{cfg['db']['table']}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
