9f2f4c163a6e2728685f3fb9383863c301918021
Maksim Leali <maksimleali@Maksims-Mac-mini.local>
2022-06-21 13:39:05 +0200

    feat: reused config fule

--- data/__init__.py
+++ data/__init__.py
@@ -1,11 +1,8 @@
 from api import app
-import os
 from flask_sqlalchemy import SQLAlchemy
 from config import cfg 
 
-print(os.getenv("DATABASE_URL", "sqlite://"))
-
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "postgresql://")
+app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{cfg['db']['user']}:{cfg['db']['password']}@{cfg['db']['host']}:{cfg['db']['port']}/{cfg['db']['table']}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
