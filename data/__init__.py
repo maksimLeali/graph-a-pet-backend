3873e7a90c718ba1fce54459754bc69c762669a0
Maksim Leali <maksimleali@Maksims-Mac-mini.local>
2022-06-21 12:11:34 +0200

    fix db init

--- data/__init__.py
+++ data/__init__.py
@@ -3,7 +3,7 @@ from os import getenv
 from flask_sqlalchemy import SQLAlchemy
 from config import cfg 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL", "postgresql://")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
