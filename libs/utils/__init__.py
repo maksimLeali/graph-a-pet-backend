3e48e8fd4159a209acd9e3f23340bf579667c59c
Maksim Leali <maksimleali@Maksims-Mac-mini.local>
2022-07-29 15:36:27 +0200

    feat: almost finished

--- libs/utils/__init__.py
+++ libs/utils/__init__.py
@@ -15,41 +15,9 @@ def format_filters(to_format):
     filters = {}
     for filter in to_format:
         filters[filter['key']] = filter['value']
-    return filters
+    return filters    
 
-def format_join(to_format):
-    join={}
-    for entity in to_format : 
-        key= entity['key']
-        join[key]= {}
-        filter_keys = py_.keys(entity['value'])
-        for f_key in filter_keys:
-            if f_key!="join": 
-                join[key][f_key]= format_filters(to_format=entity['value'][f_key])
-            else:
-                join[key][f_key]= format_join(to_format=entity['value'][f_key])
-    return join
-
-def format_common_search(common_search):
-    logger.info(f"common_search: {stringify(common_search)}")
-    return {
-        "pagination": {"page": common_search['page'],
-                       "page_size": common_search['page_size']},
-        "ordering": {
-            "order_by": common_search['order_by'],
-            "order_direction": common_search['order_direction']
-        },
-        "filters": {
-            "fixed": format_filters(common_search['filters']['fixed']),
-            "lists": format_filters(common_search['filters']['lists']),
-            "ranges": format_filters(common_search['filters']['ranges']),
-            "join": format_join(common_search['filters']['join'])
-        },
-        "search": common_search['search'],
-        "search_fields": common_search['search_fields']
-    }
-    
-def format_deep_search(deep_search): 
+def format_common_search(deep_search): 
     logger.info(f"deep_search: {stringify(deep_search)}")
     return {
         "pagination": {"page": deep_search['page'],
@@ -78,7 +46,9 @@ def format_deep_filters(filters: dict):
             if deep_key in ['and', 'or', 'not' ] :
                 filters[key][deep_key] = format_deep_filters(filters[key][deep_key])[deep_key]
             else:
-                if deep_key == 'join' : 
+                if deep_key == 'search': 
+                    filters[key][deep_key] = filters[key][deep_key]
+                elif deep_key == 'join' : 
                     join = {}
                     for entity in filters[key][deep_key] :
                         logger.info(entity)
