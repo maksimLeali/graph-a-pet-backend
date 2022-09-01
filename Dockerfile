9cf4e52cecd6f58918e95a92b1d0a195ecc47a60
Maksim Leali <maksimleali@Maksims-Mac-mini.local>
2022-09-01 11:48:51 +0200

    try on compose

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,6 @@ COPY . /app/
 
 WORKDIR /app
 RUN pip install -r requirements.txt
-CMD ["startup.sh"]
 
 
 
--- startup.sh
+++
@@ -1,7 +0,0 @@
-while ! mysqladmin ping -h"postgres" --silent; do
-  sleep 1
-done
-cd alembic
-alembic upgrade head
-cd ..
-# Start the backend...
